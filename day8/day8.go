14c1d9af3595bd83820e103924f5eff5733c408b
Erik Westra <e.s.westra.95@gmail.com>
2022-12-11 13:41:26 +0100

    Next day templates

---
+++ day8/day8.go
@@ -0,0 +1,57 @@
+package day8
+
+import (
+	"bufio"
+	"os"
+)
+
+type Solver struct {
+	Path    string
+	List    []any
+	Result1 int
+	Result2 int
+}
+
+func NewSolver(path string) *Solver {
+	solver := &Solver{
+		Path: path,
+		List: []any{},
+	}
+
+	solver.ReadListFromFile()
+
+	return solver
+}
+
+func (s *Solver) ReadListFromFile() {
+	// Read the file
+	f, err := os.Open(s.Path)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	// Create a new scanner to loop over lines
+	fs := bufio.NewScanner(f)
+
+	// Scan over each line in the file
+	for fs.Scan() {
+		text := fs.Text()
+		// Ignore whitespace/ empty lines
+		if text == " " || text == "\n" || text == "" {
+			// Go to next line
+			continue
+		}
+
+		s.List = append(s.List, text)
+	}
+}
+
+func (s *Solver) SolvePart1() {
+
+}
+
+func (s *Solver) SolvePart2() {
+
+}
---
+++ day9/day9_test.go
@@ -0,0 +1,47 @@
+package day9
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolver_SolvePart1(t *testing.T) {
+	tests := []struct {
+		name string
+		s    *Solver
+		want int
+	}{
+		{
+			name: "Example from aoc",
+			s:    NewSolver("./testdata/input.txt"),
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.s.SolvePart1()
+			assert.Equal(t, tt.want, tt.s.Result1)
+		})
+	}
+}
+
+func TestSolver_SolvePart2(t *testing.T) {
+	tests := []struct {
+		name string
+		s    *Solver
+		want int
+	}{
+		{
+			name: "Example from aoc",
+			s:    NewSolver("./testdata/input.txt"),
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.s.SolvePart2()
+			assert.Equal(t, tt.want, tt.s.Result2)
+		})
+	}
+}
